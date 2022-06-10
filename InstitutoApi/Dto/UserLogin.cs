4068f5ef584310b6304a401a9dd65021fd6675c8
Gonzalo Bonfrisco <gbonfrisco@gmail.com>
2022-06-10 09:52:32 -0300

    +Agregamos seguridad a swagger pedido de token

--- InstitutoApi/Dto/UserLogin.cs
+++ InstitutoApi/Dto/UserLogin.cs
@@ -7,7 +7,16 @@ namespace InstitutoApi.Dto
 {
     public class UserLogin
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <example>usuario</example>
         public string Username { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <example>123456</example>
         public string Password { get; set; }
     }
 }
--- InstitutoApi/Startup.cs
+++ InstitutoApi/Startup.cs
@@ -52,6 +52,37 @@ namespace InstitutoApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "InstitutoApi", Version = "v1" });
 
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+
+
                 var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
             });
